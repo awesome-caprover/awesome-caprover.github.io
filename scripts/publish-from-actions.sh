7faf3988555282dd6a5644bbe8cd7752a9fc2155
Varun Sridharan <varunsridharan23@gmail.com>
2020-11-21 14:00:04 +0530

    Updated

--- scripts/publish-from-actions.sh
+++ scripts/publish-from-actions.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo `date` >> forcebuild.date
 git add -A -f dist/**
 git commit -m 'Deploy to GitHub Pages' 
